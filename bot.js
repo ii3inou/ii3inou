c9ae5ab32dbae154456db21965c15922d270f9d6
ii3inou <62692509+ii3inou@users.noreply.github.com>
2020-03-26 09:40:55 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("692653259080597556")
+setInterval(function() {
+channel.send(`fnbfghdfbgdfgbkdjfgbjkdf`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
